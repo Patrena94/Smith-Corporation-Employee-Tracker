1c87ccd60b8bc4b2f0d451fdcdb367dc2e09093b
Vashti <patrena@sbcglobal.net>
2021-08-08 13:59:51 -0700

    initial code

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql= require("mysql2");
+const db = mysql.create.Connection(
+{
+    host: "localhost", 
+// your MYSQL username,
+user: "root",
+// Your MYSQL password
+password: "Rose98"
+database: "Smith Corporation Employee Tracker",}
+console.log("Connected to the Smith Corporation Employee Tracker database.")
+);
+
+module.exports=db;
---
+++ index.js
@@ -0,0 +1,81 @@
+const inquirer = require("inquirer");
+const db = require("./db/connection");
+
+const opt =[
+    {type: "list",
+name: "selections",
+message: "what would you like to do?",
+choices: [
+    "view All Employees", 
+    "view All Departments",
+   "View All Roles",
+   "Add Role",
+    "Add Department",
+    "update Role",
+    "Exit",
+],
+},
+];
+
+const Init =()=>
+inquirer.prompt(opt)
+.then((answer) => {
+switch(answer.selections){
+    case "view All Employees":
+        viewAllEmployees();
+        break;
+        case  "view All Departments":
+            viewAllDepartments();
+            break;
+            case "View All Roles":
+                viewAllRoles();
+                break;
+                case "Add Role":
+                    Add Role();
+                    break;
+
+                    case "Add Department":
+      
+                    Add Department();
+                        break;
+
+                        case  "update Role":
+                            update Role();
+                            case"Exit":
+                            db.end();
+                            break;
+}
+});
+};
+const viewAllEmployees = (req, res) =>{
+    const sql='SELECT first_name, last_name, role_id, role.salary, role.title, deparment.name as DEPARTMENT'
+FROM employee
+INNER JOIN role ON employee.role_id =role_id
+INNER JOIN department ON role.department_id=department_id';
+db.query(sql, (err, res)=> {
+    if(err) throw err;
+    console.table(res)
+    Init();
+})
+};
+console.log("This switch staement works fine!")
+
+const viewAllDepartments = (req, res) =>{
+    const sql='SELECT * FROM department';
+db.query(sql, (err, res) =>{
+if (err)throw err;
+console.table (res);
+Init();
+)};
+    console.log("This switch staement works fine!")
+
+    const viewAllRole = (req, res) =>{
+        const sql ='SELECT role.id. role.title, role.salary, department. name as "Department"
+FROM role
+INNER JOIN department ON role.department_id =department.id';
+db.query(sql, (err, res)=> {
+    if(err) throw err;
+    console.table(res);
+    Init();
+});
+Init();
